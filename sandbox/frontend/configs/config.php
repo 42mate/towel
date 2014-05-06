c0f60c3ae2c88fb0fc9a53a1490b8c7da120e05d
Agustin Casiva <agustin@42mate.com>
2014-05-06 16:50:46 -0300

    Added config changes to the sandbox

--- sandbox/frontend/configs/config.php
+++ sandbox/frontend/configs/config.php
@@ -1,6 +1,5 @@
 <?php
 
-//Configurations
 global $appConfig;
 
 define('APP_ROOT_DIR', dirname(__FILE__) . '/../..');
@@ -9,8 +8,9 @@ define('APP_UPLOADS_DIR', APP_WEB_DIR . '/uploads');
 define('APP_LIB_DIR', APP_ROOT_DIR . '/vendor');
 define('APP_FW_DIR', APP_ROOT_DIR . '/vendor/42mate/towel/src/Towel');
 define('APP_CONFIG_DIR', dirname(__FILE__));
-define('APP_DIR', dirname(__FILE__) . '/../Frontend');
+define('APP_DIR', dirname(__FILE__) . '/../[[YOURAPPNAME]]');
 define('APP_BASE_URL', '/');
+define('APP_SYS_EMAIL', 'your@email.com');
 
 $appConfig = array(
     'doctrine' => array(
@@ -23,6 +23,16 @@ $appConfig = array(
                 'password' => 'db_pass',
                 'charset' => 'utf8',
             ),
+            /**
+             * 'mysql_two' => array(
+             *   'driver'    => 'pdo_mysql',
+             *   'host'      => 'mysql_write.someplace.tld',
+             *   'dbname'    => 'my_database',
+             *   'user'      => 'my_username',
+             *   'password'  => 'my_password',
+             *   'charset'   => 'utf8',
+             *   ),
+             */
         ),
     ),
 
@@ -37,6 +47,10 @@ $appConfig = array(
         'name' => '_SESS_APP',
         'cookie_lifetime' => 0,
         'session.storage.save_path' => '/tmp/',
+    ),
+
+    'class_map' => array(
+        'user' => '\Frontend\Model\User'
     ),
 
     'debug' => true,
