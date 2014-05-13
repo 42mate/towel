965cebcd8164670d6718ef6526b9ee298a6bfecf
Ezequiel Pintos <ezequiel@42mate.com>
2014-05-13 18:15:43 -0300

    Bye bye sandbox

--- sandbox/frontend/bootstrap.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-require_once dirname(__FILE__) . '/../vendor/autoload.php';
-require_once dirname(__FILE__) . '/configs/config.php';
-require_once APP_FW_DIR . '/bootstrap.php';
--- sandbox/install-composer.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -sS https://getcomposer.org/installer | php
-
