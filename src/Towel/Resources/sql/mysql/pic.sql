0db19c056670b53940ea8a0d4f212b52b91c2b78
agustin <agustin@42mate.com>
2015-01-25 10:13:55 -0300

    Renaming folders

--- src/Towel/Resources/sql/mysql/pic.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE pic (
-  id BIGINT NOT NULL AUTO_INCREMENT,
-  pic VARCHAR(255) NOT NULL,
-  created INT NOT NULL,
-  object_id BIGINT NOT NULL,
-  object_type VARCHAR(50) NOT NULL,
-  PRIMARY KEY (id)
-);
