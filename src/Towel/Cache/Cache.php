cf041ae16d5096af0c1b5637766bce7b470e1570
agustin <agustin@42mate.com>
2015-01-25 10:15:48 -0300

    Added cache again, accidentally deleted

---
+++ src/Towel/Cache/Cache.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ezequiel
+ * Date: 5/26/14
+ * Time: 4:56 PM
+ */
+
+namespace Towel\Cache;
+
+
+class Cache
+{
+    const DEFAULT_KEY_PREFIX = 'towel';
+
+    public function __construct(\Towel\Cache\CacheInterface $cacheDriver)
+    {
+        $this->cacheDriver = $cacheDriver;
+    }
+
+    public function get($key)
+    {
+        return $this->cacheDriver->get($this->getKey($key));
+    }
+
+    public function set($key, $value, $expire = null)
+    {
+        $this->cacheDriver->set($this->getKey($key), $value, $expire);
+    }
+
+    public function clear()
+    {
+        $keys = $this->getDriverInstance()->getAllKeys();
+        if (is_array($keys)) { //Bool will be returned in case of no keys, nothing to delete.
+            $this->getDriverInstance()->deleteMulti($keys);
+        }
+    }
+
+    public function delete($key)
+    {
+        $this->cacheDriver->delete($this->getKey($key));
+    }
+
+    public function getDriverInstance()
+    {
+        return $this->cacheDriver;
+    }
+
+    public function getKey($key) {
+        $prefix = self::DEFAULT_KEY_PREFIX;
+        $config = get_app()->config();
+        if (!empty($config['cache']['prefix'])) {
+            $prefix = $config['cache']['prefix'];
+        }
+
+        return $prefix . '_' . $key;
+    }
+}
